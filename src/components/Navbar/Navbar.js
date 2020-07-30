e84d1fc596abbfcecd1024cb0968fb825ebb1865
mukesh purohit <mpurohit88@gmail.com>
2020-07-30 09:12:46 +0530

    react router dom added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Navbar, Nav, Form, FormControl } from '../BootstrapWrap';
 import OutlineButton from '../Button/OutlineButton';
 
-const NavbarWrap = ({ setIsAuthenticated, setCurrentPage }) => {
+import './navbar.css';
+
+const NavbarWrap = ({ setIsAuthenticated }) => {
   return <>
     <Navbar bg="primary" variant="dark">
       <Navbar.Brand href="#home">Navbar</Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link href="#home" onClick={() => setCurrentPage(1)}>Home</Nav.Link>
-        <Nav.Link href="#courses" onClick={() => setCurrentPage(2)}>Courses</Nav.Link>
-        <Nav.Link href="#students" onClick={() => setCurrentPage(3)}>Students</Nav.Link>
+        {/* <Nav.Link href="#home" onClick={() => setCurrentPage(1)}>Home</Nav.Link>
+        <Nav.Link href="#course" onClick={() => setCurrentPage(2)}>Courses</Nav.Link>
+        <Nav.Link href="#student" onClick={() => setCurrentPage(3)}>Students</Nav.Link> */}
+
+        <Link to="/">Home</Link>
+        <Link to="/course">Course</Link>
+        <Link to="/student">Student</Link>
+
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
--- src/containers/Admin/Home.js
+++ src/containers/Admin/Home.js
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
 
+import { } from 'react-router-dom'; // react-router-native
+
 import NavbarWrap from '../../components/Navbar/Navbar'
 import Dashboard from './Dashboard'
 import Student from './Student'
 import Course from './Course/Course'
+import { Switch, Route } from 'react-router-dom';
 
 const Home = ({ setIsAuthenticated }) => {
-  const [currentPage, setCurrentPage] = useState(1);
+  // const [currentPage, setCurrentPage] = useState(1);
 
-  const getPage = () => {
-    switch (currentPage) {
-      case 1:
-        return <Dashboard />;
-      case 2:
-        return <Course />;
-      case 3:
-        return <Student />;
-      default:
-        return <Dashboard />;
-    }
-  }
+  // const getPage = () => {
+  //   switch (currentPage) {
+  //     case 1:
+  //       return <Dashboard />;
+  //     case 2:
+  //       return <Course />;
+  //     case 3:
+  //       return <Student />;
+  //     default:
+  //       return <Dashboard />;
+  //   }
+  // }
 
   return <>
-    <NavbarWrap setIsAuthenticated={setIsAuthenticated} setCurrentPage={setCurrentPage} />
+    <NavbarWrap setIsAuthenticated={setIsAuthenticated} />
     {
-      getPage()
+      // getPage()
       // props.children
     }
+
+    <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route path="/course" component={Course} />
+      <Route path="/student" component={Student} />
+    </Switch>
   </>
 }
 
